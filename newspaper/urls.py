0b8de0483ff6c5029ecd5916b4918305b84a25f9
sergey <38cerg12021979@jmal.com>
2024-06-13 14:57:01 +0300

    add the track name from redactor_list to redactor_list.html

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -4,12 +4,14 @@ from newspaper.views import (
     index,
     TopicListView,
     NewspaperListView,
+    RedactorListView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("topics_list/", TopicListView.as_view(), name="topics-list"),
     path("newspaper_list/", NewspaperListView.as_view(), name="newspapers-list"),
+    path("redactor_list/", RedactorListView.as_view(), name="redactors-list"),
 
 
 ]
