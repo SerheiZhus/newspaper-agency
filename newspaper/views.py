bafd69d80a31296aba6ae8198710faf541f71013
sergey <38cerg12021979@jmal.com>
2024-06-13 08:34:27 +0300

    add function index

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from newspaper.models import (
+    Redactor,
+    Topic,
+    Newspaper,
+)
 
-# Create your views here.
+
+def index(request):
+
+    num_redactor = Redactor.objects.count()
+    num_topic = Topic.objects.count()
+    num_newspaper = Newspaper.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactor": num_redactor,
+        "num_topic": num_topic,
+        "num_newspaper": num_newspaper,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(
+        request,
+        "newspaper/index.html",
+        context=context
+    )
