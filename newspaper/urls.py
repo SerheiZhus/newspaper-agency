630dc592d6a62a01824442fa5b9e12eea27c0c9d
sergey <38cerg12021979@jmal.com>
2024-06-20 10:19:56 +0300

    add the track name from topics-detail to topic_detail.html

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -3,13 +3,16 @@ from django.urls import path
 from newspaper.views import (
     index,
     TopicListView,
+    TopicDetailView,
     NewspaperListView,
     RedactorListView,
+
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("topics_list/", TopicListView.as_view(), name="topics-list"),
+    path("topics_list/<int:pk>", TopicDetailView.as_view(), name="topics-detail"),
     path("newspaper_list/", NewspaperListView.as_view(), name="newspapers-list"),
     path("redactor_list/", RedactorListView.as_view(), name="redactors-list"),
 
